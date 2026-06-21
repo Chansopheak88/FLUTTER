5ba76b3bfaa25c18b2f3b50d311b8a98702b61a3
Chansopheak88 <sopheak.chan@student.cadt.edu.kh>
2026-06-21 20:18:07 +0700

    exercise1 commit

---
+++ flutter_practice/lib/week7_exercise/TemperatureApp/screen/temperature_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class TemperatureScreen extends StatefulWidget {
+  const TemperatureScreen({super.key});
+
+  @override
+  State<TemperatureScreen> createState() => _TemperatureScreenState();
+}
+
+class _TemperatureScreenState extends State<TemperatureScreen> {
+  final TextEditingController textController = TextEditingController();
+
+  String get fahrenheit {
+    double? val = double.tryParse(textController.text);
+    if (val == null) {
+      return "No value";
+    }
+    double fahrenheit = (val * 2) + 30;
+    return "$fahrenheit F";
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    textController.dispose();
+  }
+
+  final BoxDecoration textDecoration = BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(12),
+  );
+
+  final InputDecoration inputDecoration = InputDecoration(
+    enabledBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.white, width: 1.0),
+      borderRadius: BorderRadius.circular(12),
+    ),
+    hintText: 'Enter a temperature',
+    hintStyle: const TextStyle(color: Colors.white),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Icon(
+              Icons.thermostat_outlined,
+              size: 120,
+              color: Colors.white,
+            ),
+            const Center(
+              child: Text(
+                "Converter",
+                style: TextStyle(color: Colors.white, fontSize: 45),
+              ),
+            ),
+            const SizedBox(height: 50),
+            const Text("Temperature in Degrees:"),
+            const SizedBox(height: 10),
+            TextField(
+              controller: textController,
+              onChanged: (value) => setState(() {}),
+              decoration: inputDecoration,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 30),
+            const Text("Temperature in Fahrenheit:"),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: textDecoration,
+              child: Text(fahrenheit),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
